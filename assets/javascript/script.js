484f0cddca1665bca23ff92781c07466407eb678
Stama Eleazar <eleoatam10@gmail.com>
2019-07-08 23:18:32 -0500

    btn works

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
 
     var timeRunning = false;
     var timeleft = 30;
+    var correct = 0;
+    var incorrect = 0;
+
+
 
 
 
@@ -25,12 +29,16 @@ $(document).ready(function(){
 
      timeleft--;
 
+     
+     
+
     if(timeleft <= 0){
 
     clearInterval(startTimer);
  
 
     $(".countdown").text("Finished!!")
+    result()
 
 
     }
@@ -38,8 +46,149 @@ $(document).ready(function(){
 
     timeRunning = true;
 
+    
+    qone()
 
     }
+
+    ///Q1
+
+    function qone(){
+
+         ///Q1
+        $('.btno').on('click',function(){
+            correct++
+            console.log(correct)
+            $(this).attr('disabled', true)
+            $('.btd').attr('disabled', true)
+
+
+        })
+        $('.btd').on('click',function(){
+            incorrect++
+            console.log(incorrect)
+            $(this).attr('disabled', true)
+            $('.btno').attr('disabled', true)
+
+
+
+        })
+
+         ///Q2
+        $('.btnt').on('click',function(){
+            incorrect++
+            console.log(incorrect)          
+
+
+        })
+        $('.btf').on('click',function(){
+            
+            correct++
+            console.log(correct)
+
+
+        })
+
+         ///Q3
+        $('.btnf').on('click',function(){
+            
+            correct++
+            console.log(correct)
+
+
+        })
+        $('.btns').on('click',function(){
+            incorrect++
+            console.log(incorrect)          
+
+
+        })
+
+
+
+         ///Q4
+
+         $('.bts').on('click',function(){
+            incorrect++
+            console.log(incorrect)          
+
+
+        })
+        $('.bte').on('click',function(){
+            
+            correct++
+            console.log(correct)
+
+
+        })
+
+         ///Q5
+
+         $('.btnn').on('click',function(){
+
+            correct++
+            console.log(correct)
+
+               
+
+
+        })
+        $('.btt').on('click',function(){
+            incorrect++
+            console.log(incorrect)       
+            
+
+        })
+
+         ///Q6
+
+         $('.btnee').on('click',function(){
+
+            correct++
+            console.log(correct)
+
+               
+
+
+        })
+        $('.bttt').on('click',function(){
+            incorrect++
+            console.log(incorrect)       
+            
+
+        })
+
+         ///Q7
+
+         $('.btnff').on('click',function(){
+            result()
+
+            correct++
+            console.log(correct)
+
+               
+
+
+        })
+        $('.btff').on('click',function(){
+
+            result()
+            incorrect++
+            console.log(incorrect)       
+            
+
+        })
+
+
+
+
+    }
+
+    function result(){
+        $('.correcto').text('Correct Answers: ' + correct)
+        $('.incorrecto').text('Incorrect Answers: ' + incorrect)
+    }
+    
 
 }
 
