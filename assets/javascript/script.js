3fc1599d7d1130c739c3615f74e99df133f3279e
Stama Eleazar <eleoatam10@gmail.com>
2019-07-08 21:46:44 -0500

    timer working completely

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,47 @@
-
 $(document).ready(function(){
 
     $(".start").on("click", start);
 
 
+    var timeRunning = false;
+    var timeleft = 30;
 
-  function start() {
-      console.log('it works')
-      
-  }
+
+
+
+
+    function start() {
+
+        if (!timeRunning) {
+            
+            clockRunning = true;
+        
+         
+
+    var startTimer = setInterval(function(){
+
+
+    $(".countdown").text("Time Remaining: "+ timeleft + " seconds")
+
+     timeleft--;
+
+    if(timeleft <= 0){
+
+    clearInterval(startTimer);
+ 
+
+    $(".countdown").text("Finished!!")
+
+
+    }
+    }, 1000);
+
+    timeRunning = true;
+
+
+    }
+
+}
 
 
 
