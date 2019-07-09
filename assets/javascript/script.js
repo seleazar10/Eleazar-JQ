545e0eb7fbc068fcdeab18526cec873008ca0c76
Stama Eleazar <eleoatam10@gmail.com>
2019-07-09 00:02:55 -0500

    animations  added to buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.contwo').hide()
     $('.conthree').hide()
-    $('#buttons').fadeOut().fadeIn(3000)
-   
+    $('#buttons').fadeOut().fadeIn(1000).fadeOut(500).fadeIn(1000)
 
+ 
 
-    $(".start").on("click", function(){
+    $(".start").on("click", function () {
         start();
         $('.contwo').toggle(1000)
-        
+
 
     });
 
 
     var timeRunning = false;
-    var timeleft = 30;
+    var timeleft = 10;
     var correct = 0;
     var incorrect = 0;
 
 
 
-
+    function result() {
+        $('.conthree').toggle()
+        $('.contwo').hide()
+        $('.correcto').text('Correct Answers: ' + correct)
+        $('.incorrecto').text('Incorrect Answers: ' + incorrect)
+    }
 
 
 
@@ -29,188 +34,200 @@ $(document).ready(function(){
 
         
 
+
+
+
         if (!timeRunning) {
-            
+
             clockRunning = true;
-        
-         
-
-    var startTimer = setInterval(function(){
-
-
-    $(".countdown").text("Time Remaining: "+ timeleft + " seconds")
-
-     timeleft--;
-
-     
-
-     
-     
-
-    if(timeleft <= 0){
-
-    clearInterval(startTimer);
- 
-
-    $(".countdown").text("Finished!!")
-    $('.conthree').toggle()
-    $('.contwo').toggle()
-
-
-    }
-    }, 1000);
-
-    timeRunning = true;
-
-    
-    qone()
-
-    }
-
-    ///Q1
-
-    function qone(){
-
-         ///Q1
-        $('.btno').on('click',function(){
-            correct++
-            console.log(correct)
-            $(this).attr('disabled', true)
-            $('.btd').attr('disabled', true)
-
-
-        })
-        $('.btd').on('click',function(){
-            incorrect++
-            console.log(incorrect)
-            $(this).attr('disabled', true)
-            $('.btno').attr('disabled', true)
 
 
 
-        })
-
-         ///Q2
-        $('.btnt').on('click',function(){
-            incorrect++
-            console.log(incorrect)          
+            var startTimer = setInterval(function () {
 
 
-        })
-        $('.btf').on('click',function(){
-            
-            correct++
-            console.log(correct)
+                $(".countdown").text("Time Remaining: " + timeleft + " seconds")
 
-
-        })
-
-         ///Q3
-        $('.btnf').on('click',function(){
-            
-            correct++
-            console.log(correct)
-
-
-        })
-        $('.btns').on('click',function(){
-            incorrect++
-            console.log(incorrect)          
-
-
-        })
+                timeleft--;
 
 
 
-         ///Q4
-
-         $('.bts').on('click',function(){
-            incorrect++
-            console.log(incorrect)          
 
 
-        })
-        $('.bte').on('click',function(){
-            
-            correct++
-            console.log(correct)
+
+                if (timeleft <= 0) {
+
+                    
+
+                    clearInterval(startTimer);
+                    
 
 
-        })
 
-         ///Q5
+                    $(".countdown").text("Finished!!")
+                    result()
 
-         $('.btnn').on('click',function(){
+                    
 
-            correct++
-            console.log(correct)
+
+                }
+
 
                
 
+            }, 1000);
 
-        })
-        $('.btt').on('click',function(){
-            incorrect++
-            console.log(incorrect)       
-            
-
-        })
-
-         ///Q6
-
-         $('.btnee').on('click',function(){
-
-            correct++
-            console.log(correct)
-
-               
+            timeRunning = true;
 
 
-        })
-        $('.bttt').on('click',function(){
-            incorrect++
-            console.log(incorrect)       
-            
+            qone()
 
-        })
+        }
 
-         ///Q7
+        ///Q1
 
-         $('.btnff').on('click',function(){
-          
+        function qone() {
 
-            correct++
-            console.log(correct)
-            result()
-
-               
+            ///Q1
+            $('.btno').on('click', function () {
+                correct++
+                console.log(correct)
+                $(this).attr('disabled', true)
+                $('.btd').attr('disabled', true)
 
 
-        })
-        $('.btff').on('click',function(){
-
-            
-            incorrect++
-            console.log(incorrect) 
-            result()      
-            
-
-        })
+            })
+            $('.btd').on('click', function () {
+                incorrect++
+                console.log(incorrect)
+                $(this).attr('disabled', true)
+                $('.btno').attr('disabled', true)
 
 
 
+            })
 
-    }
+            ///Q2
+            $('.btnt').on('click', function () {
+                incorrect++
+                console.log(incorrect)
 
-    function result(){
-        $('.conthree').toggle()
+
+            })
+            $('.btf').on('click', function () {
+
+                correct++
+                console.log(correct)
+
+
+            })
+
+            ///Q3
+            $('.btnf').on('click', function () {
+
+                correct++
+                console.log(correct)
+
+
+            })
+            $('.btns').on('click', function () {
+                incorrect++
+                console.log(incorrect)
+
+
+            })
+
+
+
+            ///Q4
+
+            $('.bts').on('click', function () {
+                incorrect++
+                console.log(incorrect)
+
+
+            })
+            $('.bte').on('click', function () {
+
+                correct++
+                console.log(correct)
+
+
+            })
+
+            ///Q5
+
+            $('.btnn').on('click', function () {
+
+                correct++
+                console.log(correct)
+
+
+
+
+            })
+            $('.btt').on('click', function () {
+                incorrect++
+                console.log(incorrect)
+
+
+            })
+
+            ///Q6
+
+            $('.btnee').on('click', function () {
+
+                correct++
+                console.log(correct)
+
+
+
+
+            })
+            $('.bttt').on('click', function () {
+                incorrect++
+                console.log(incorrect)
+
+
+            })
+
+            ///Q7
+
+            $('.btnff').on('click', function () {
+
+
+                correct++
+                console.log(correct)
+                result()
+
+
+
+
+            })
+            $('.btff').on('click', function () {
+
+
+                incorrect++
+                console.log(incorrect)
+                result()
+
+
+            })
+
+
+
+
+        }
+
+        function result() {
+            $('.conthree').toggle()
             $('.contwo').hide()
-        $('.correcto').text('Correct Answers: ' + correct)
-        $('.incorrecto').text('Incorrect Answers: ' + incorrect)
-    }
-    
+            $('.correcto').text('Correct Answers: ' + correct)
+            $('.incorrecto').text('Incorrect Answers: ' + incorrect)
+        }
 
-}
+
+    }
 
 
 
